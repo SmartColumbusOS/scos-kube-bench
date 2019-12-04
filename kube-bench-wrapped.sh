3be37010686f4c8e783e31e2c186a6d296281e44
cameronmarsh <cmarsh@pillartechnology.com>
2019-12-04 09:21:37 -0500

    Simplified kube-bench wrapper script. Cleaned up job yaml
    smartcolumbusos/common#437
    
    co-authored-by: Kenny Drobnack <kdrobnack@pillartechnology.com>

--- kube-bench-wrapped.sh
+++ kube-bench-wrapped.sh
@@ -4,10 +4,5 @@ kube-bench --version 1.16 > report.out
 cat report.out
 failures=$(cat report.out | grep '\[FAIL\]' | /usr/bin/wc -l )
 
-if [ $failures != 0 ]; then
-  exit 1
-else
-  exit 0
-fi
-
+exit $failures
 
